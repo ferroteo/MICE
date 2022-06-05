a3fde2ca2d62c3e9e5fb5b7cb882aa6e0acf6473
ferroteo <ferro.teo98@gmail.com>
2022-06-05 13:50:54 +0200

    complated mode median and mean imputation

---
+++ main.R
@@ -0,0 +1,92 @@
+
+#_________________
+####  SET UP  ####
+#_________________
+
+# renv::init()
+# renv::activate()
+
+library(mice)
+library(MASS)
+
+# renv::settings$snapshot.type("all")
+
+
+
+
+#_______________
+####  FUNC  ####
+#_______________
+
+
+# Define all the function used in the code
+
+MeanImputation = function(vec){
+  vec[is.na(vec)] = mean( vec[!is.na(vec)] ) 
+  return(vec)
+}
+
+MedianImputation = function(vec){
+  vec[is.na(vec)] = median( vec[!is.na(vec)] ) 
+  return(vec)
+}
+
+ModeImputation = function(vec){
+  not_na = vec[!is.na(vec)]
+  freq_table = as.data.frame(table(not_na))
+  pos = which.max(freq_table$Freq)
+  vec[is.na(vec)] = as.numeric(levels(freq_table$not_na)[pos])
+  return(vec)
+}
+
+
+#_______________
+####  CODE  ####
+#_______________
+
+
+# DATASET
+df_miss = airquality
+
+
+##### Mean imputation  ####
+
+
+#save inplace 
+for(col in colnames(df_miss)){
+  df_miss[col] = MeanImputation(df_miss[col])
+}
+
+#save in new dataframe 
+df_mean = data.frame(
+  apply(df_miss, 2, FUN = MeanImputation)
+)
+
+
+##### Median imputation  ####
+
+#save inplace 
+for(col in colnames(df_miss)){
+  df_miss[col] = MedianImputation(df_miss[col])
+}
+
+#save in new dataframe 
+df_median = data.frame(
+  apply(df_miss, 2, FUN = MedianImputation)
+)
+
+
+##### Mode imputation  ####
+
+#save inplace 
+for(col in colnames(df_miss)){
+  df_miss[col] = ModeImputation(df_miss[col])
+}
+
+#save in new dataframe 
+df_mode = data.frame(
+  apply(df_miss, 2, FUN = ModeImputation)
+)
+
+
+
